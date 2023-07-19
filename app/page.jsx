6cb58a29cd8efc447beab0e3172edc1f1b85769f
Eugeniusz <104994034+eugeniusz57@users.noreply.github.com>
2023-07-19 19:40:26 +0200

    create page Home and add style

---
+++ app/page.jsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+import Button from "@/components/Button/Button";
+import Hero from "public/hero.png";
+
+export default function Home() {
+  return (
+    <main className={styles.container}>
+      <div className={styles.item}>
+        <h1 className={styles.title}>The future of AI in the next few years</h1>
+        <p>
+          Turning your Idea into Reality. We bring together the teams from the
+          global tech industry.
+        </p>
+        <Button url="/portfolio" text="See Our Works" />
+      </div>
+      <div className={styles.item}>
+        <Image src={Hero} alt="main photo" className={styles.img} />
+      </div>
+    </main>
+  );
+}
